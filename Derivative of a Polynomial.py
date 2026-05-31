35d24c4010420506e47eb208525be445ac67dd3a
Ometh292 <ometh@idea8.solutions>
2026-05-31 21:23:29 +0530

    Update and rename Derivative of a Polynomial.md to Derivative of a Polynomial.py

--- Derivative of a Polynomial.py
+++ Derivative of a Polynomial.py
@@ -1,3 +1,10 @@
+'''
 Implement a function that computes the derivative of a polynomial term of the form 
 c * x^n at a given point x, where c is a coefficient and n is the exponent. The function should return the value of the derivative, accounting for the coefficient in the power rule. 
 This is useful for understanding how polynomials change at specific points in machine learning optimization problems.
+'''
+
+def poly_term_derivative(c: float, x: float, n: float) -> float:
+    
+    return c*n*x**(n-1)
+
